7492ba08eafb3834ec4e9668418baf6ab0c32e0c
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-06 16:21:16 +0200

    21 th module

---
+++ 21. Random number generator/HelloWorld.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ctime>
+
+
+int main() {
+
+
+
+    srand(time(NULL));
+
+    int num1 =(rand() % 6) + 1;
+    int num2 =(rand() % 6) + 1;
+    int num3 =(rand() % 6) + 1;
+
+
+    std::cout << num1 << std::endl;
+    std::cout << num2 << std::endl;
+    std::cout << num3 << std::endl;
+
+   return 0 ;
+}
+
+
