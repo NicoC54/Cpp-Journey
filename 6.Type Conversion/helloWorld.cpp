f2bde0ff297cf8c4bb2d31a12722d4b59d9ec597
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-06 10:56:51 +0200

    seventh module

---
+++ 6.Type Conversion/helloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+  
+
+   int correct = 8;
+   int questions = 10;
+   double score = correct/questions * 100;
+
+   std::cout << score <<"%";
+
+   return 0;
+
+}
---
+++ 7.User input/helloworld.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+    std::string name;
+    int age;
+
+    std::cout << "What's your age ?: ";
+    std::cin >> age;
+
+    std::cout << "What's your full name ?: ";
+    std::getline(std::cin >> std::ws, name);
+    std::cout << "Hello " << name;
+
+   
+
+    std::cout << "Hello "<< name;
+    std::cout << "You are " << age << " years old";
+
+    return 0;
+
+}
