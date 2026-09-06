fb1d772c67d1785bd64cf95da39a802dc80835c4
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-06 11:15:37 +0200

    Add fifth module

---
+++ 5.Arithmetic Operators/helloworld.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+
+
+int main() {
+
+    double students = 21;
+
+    std::cout<<students<<std::endl;
+    students+=1;
+    std::cout<<students<<std::endl;
+    students++;
+    std::cout<<students<<std::endl;
+    students*=4;
+    std::cout<<students<<std::endl;
+    students--;
+    std::cout<<students<<std::endl;
+    students-=5;
+    std::cout<<students<<std::endl;
+    students/=3;
+    std::cout<<students<<std::endl;
+    
+
+
+   return 0;
+
+}
