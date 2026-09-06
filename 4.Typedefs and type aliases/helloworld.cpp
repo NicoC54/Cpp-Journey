f9dad6c96c8dc35bc156cb18a936a10ce320ee44
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-06 09:45:05 +0200

    Fourth Module

---
+++ 4.Typedefs and type aliases/helloworld.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+
+ //typedef std::vector<std::pair<std::string, int >> pairlist_t ;
+ //typedef std::string text_t;
+ //typedef int number_t;
+
+ using text_t = std::string;
+ using number_t = int;
+  
+
+int main() {
+
+
+    text_t firstName = "Bro";
+
+    number_t age = 15;
+
+    std::cout << firstName << std::endl;
+    std::cout << age << std::endl;
+    std::cout << age << '\n';
+
+
+   return 0;
+
+}
