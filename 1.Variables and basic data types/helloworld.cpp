03c9c2f43108cc757bf6e01c13b6e35aa227bda6
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-06 09:01:40 +0200

    First Module

---
+++ 1.Variables and basic data types/helloworld.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+int main() {
+
+
+    
+    int age = 23;
+    char grade = 'A';
+    std::string name = "Nicolas";
+    std::string adress = "123 test espace";
+    
+
+
+    bool student = true;
+
+    std::cout << "Hello " << name << " you live in " << adress << std :: endl;
+    std::cout << "congrats, you got an " << grade << " for your last exam";
+    return 0;
+
+}
