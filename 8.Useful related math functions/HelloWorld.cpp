1c3307a899994927df3b1ab81217727b0259c31f
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-06 11:33:44 +0200

    eighth module

---
+++ 8.Useful related math functions/HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+
+    int sideA = 10;
+    int sideB = 5;
+    float hypothenuse = sqrt(pow(sideA,2) + pow(sideB,2));
+
+    std::cout << "The hypothenuse is: " << hypothenuse << std::endl;
+
+    return 0;
+
+}
