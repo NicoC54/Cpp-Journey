8c456b8431d174e41245aed5a9a5308aa0d79667
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-06 13:34:21 +0200

    eleventh module

---
+++ 11. Switch statements/HelloWorld.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+
+    int month;
+
+    std::cout << "enter the month(1-2)";
+    std::cin >> month;
+
+    switch(month){
+        case 1:
+            std::cout<<"its january";
+            break;
+      
+        case 2:
+            std::cout << "its february";
+            break;
+              }
+
+
+    return 0;
+
+}
+
