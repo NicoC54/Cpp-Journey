9616c295445a6568134d3f7d06628426484a241b
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-06 15:31:06 +0200

    16th module

---
+++ 16. Infinite While loop/HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    
+    std::string name;
+
+    while(true){
+
+        std::cout << "IM STUCK IN AN INFINITE LOOP HELP";
+    }
+
+    return 0;
+
+
+}
+
