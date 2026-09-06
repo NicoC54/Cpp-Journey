3b341b74907ca9da6f4c57ae805177b5e38b8b0c
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-06 14:46:46 +0200

    13th module

---
+++ 13.Ternary operator/HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    
+    bool hungry = false;
+
+    hungry ? std::cout<<"you are hungry" : std::cout<<"you are full";
+
+
+    return 0;
+
+}
+
