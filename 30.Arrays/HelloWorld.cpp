9787c8e9e990088ecbac979be03c9b0756007453
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-07 11:16:19 +0200

    30 th module

---
+++ 30.Arrays/HelloWorld.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+
+int main() {
+
+
+    std::string car[] = {"Corvette", "Mustant", "ford"};
+
+    car[0]="Camaro";
+
+
+    std::cout << car[0];
+    std::cout << car[1];
+    std::cout << car[2];
+
+        
+
+
+    double prices[3];
+
+    prices[0] = 10;
+    
+    prices[1] = 1000486;
+    
+    prices[2] = 10.642121;
+
+    std::cout << prices[1];
+
+    return 0;
+    
+
+}
