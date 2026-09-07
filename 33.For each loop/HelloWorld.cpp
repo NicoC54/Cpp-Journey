badc9e41750474ff2417fb96784ac3c5b9d68476
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-07 11:35:13 +0200

    33th Module

---
+++ 33.For each loop/HelloWorld.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+int main() {
+
+   // std::string students[] = {"spongebob","patrick","Nicopops","Sandy"};
+
+    //for(std::string student : students ) {
+    //    std::cout << student << '\n';
+    //}
+    
+    //return 0
+    
+    int grades[] = {65,72,81,93};
+
+    for (int number : grades){
+        std::cout << number <<std::endl;
+    }
+
+
+
+
+
+}
