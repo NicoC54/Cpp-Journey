6b11facc96da6bdb645594e5fb18ae22d85be067
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-07 14:45:56 +0200

    37th module

---
+++ 37.fill function/HelloWorld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+    const int SIZE = 100;
+    std::string foods[SIZE];
+
+    fill(foods, foods + SIZE/2, "pizza");
+    fill(foods + SIZE/2, foods + SIZE, "hamburger");
+
+    for (std::string food : foods){
+        std::cout << food << '\n';
+    }
+  
+    return 0;
+}
+
+
