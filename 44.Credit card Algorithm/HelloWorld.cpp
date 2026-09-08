9c7802db960b7eb4637e51b28eb999b2f3001850
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-08 08:39:30 +0200

    44th module

--- 44.Credit card Algorithm/HelloWorld.cpp
+++ 44.Credit card Algorithm/HelloWorld.cpp
@@ -1,51 +1,52 @@
 #include <iostream>
 #include <string> // Oubli corrigé
 
-int sumOddIndex(const std::string &cardNumber);
-int sumPairIndex(const std::string &cardNumber);
+int getDigit(const int cardNumber);
+int sumOddDigits(const std::string cardNumber);
+int sumEvenDigits(const std::string cardNumber);
 
 int main() {
-    std::string carte;
-    std::cout << "Entrez un numero de carte : ";
-    std::cin >> carte;
 
-    // On appelle les fonctions et on stocke leurs résultats dans le main
-    int totalImpair = sumOddIndex(carte);
-    int totalPair = sumPairIndex(carte);
+    std::string cardNumber;
+    int result =0;
 
-    // L'addition finale et la vérification modulo 10
-    if ((totalImpair + totalPair) % 10 == 0) {
-        std::cout << "Cette carte est VALIDE.\n";
-    } else {
-        std::cout << "Cette carte est INVALIDE.\n";
+    std::cout << "Enter a credit card#: ";
+    std::cin >> cardNumber;
+
+    result = sumEvenDigits(cardNumber) + sumOddDigits(cardNumber);
+
+    if (result %10==0){
+        std::cout << cardNumber <<" is valid";
     }
+    else{
+         std::cout << cardNumber <<" is not valid";
+    }
+  
 
     return 0;
 }
-int sumOddIndex(const std::string &cardNumber){
-    int oddSum=0;
-    for (int i = cardNumber.size()-1; i>=0; i-=2){
-        int number = cardNumber[i] - '0';
-        oddSum += number;
-    }
-    return oddSum;
-  }
 
-int sumPairIndex(const std::string &cardNumber){
-    int PairSum =0;
-     for (int i = cardNumber.size()-2; i>=0; i-=2){
-        int number = cardNumber[i] - '0';
-        if (number*2 >=10){
-            std::string text_number = std::to_string(number*2);
-            int number1=text_number[0] - '0';
-            int number2=text_number[1] - '0';
-            int sum = number1 + number2;
-            PairSum += sum;
-        }
-        else{
-        PairSum += number*2;
-        }
+
+int getDigit(const int number){
+    return number % 10 + (number/ 10 % 10);
+
+}
+int sumOddDigits(const std::string cardNumber){
+    
+    int sum=0;
+
+    for (int i = cardNumber.size()-1; i>=0; i-=2){
+        sum+= cardNumber[i] - '0';
     }
-    return PairSum;
-  }
+    return sum ;
+
+}
+int sumEvenDigits(const std::string cardNumber){
+
+    int sum = 0;
+    for(int i = cardNumber.size() - 2; i >=0; i-=2){
+        sum+= getDigit((cardNumber[i])*- '0')*2;
+    }
+    return sum;
+}
 
