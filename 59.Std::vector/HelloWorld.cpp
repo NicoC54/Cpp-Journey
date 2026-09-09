c073e6a58dfd2530fe060c6a2728ab614de57748
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-09 09:01:14 +0200

    59th module

---
+++ 59.Std::vector/HelloWorld.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+
+
+
+struct Vertex
+{
+    float x,y,z;
+
+};
+
+
+std::ostream& operator << (std::ostream& stream, const Vertex& vertex){
+    stream << vertex.x << ", "<<vertex.y << ", "<< vertex.z;
+    return stream;
+}
+
+
+int main() {
+
+    std::vector<Vertex> vertices;
+
+    vertices.push_back({1,2,3});
+    vertices.push_back({4,5,6});
+
+   
+
+    std::cin.get();
+
+
+    vertices.erase(vertices.begin() + 1);
+
+    
+    for (const Vertex& v : vertices){
+        std::cout << v <<std::endl;
+        }
+
+
+    return 0;
+}
+
