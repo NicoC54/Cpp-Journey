29a10e2c1c6becc6ffeb5cbd59a77a2b981fa5d0
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-15 09:34:12 +0200

    Add Code exemple for mutex

---
+++ 80.Mutex/exemple1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+
+std::mutex carMutex;
+
+
+
+void driveCar(std::string driverName){
+    std::lock_guard<std::mutex> lock(carMutex);
+    std::cout << driverName << " is driving " << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    std::cout << driverName << " is done driving " << std::endl;
+}
+
+int main(){
+
+    std::thread thread1(driveCar, "saldina");
+    std::thread thread2(driveCar, "roberto");
+
+    thread1.join();
+    thread2.join();
+
+
+
+
+
+}
